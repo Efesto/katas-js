8ecef51cbdab72113e15b88346dab4479027b45a
MarcoPolita <marco.polita@helpling.com>
2017-05-20 17:18:24 +0200

    add eslint with rules

--- src/BowlingGameKata.js
+++ src/BowlingGameKata.js
@@ -2,50 +2,51 @@
 const random = require('lodash.random');
 
 function Game() {
-    this.pinsHit = () => {
-        return random(0, 10)
-    };
+	this.pinsHit = () => {
+		return random(0, 10);
+	};
 
-    this.roll = () => {
-        let pinsHit = this.pinsHit();
-        this.rollScores.push(pinsHit);
-    };
+	this.roll = () => {
+		let pinsHit = this.pinsHit();
+		this.rollScores.push(pinsHit);
+	};
 
-    this.score = () => {
-        let score = 0;
-        let rScoreIndex = 0;
-        let lastRollInFrame = false;
+	this.score = () => {
+		let score = 0;
+		let rScoreIndex = 0;
+		let lastRollInFrame = false;
 
-        this.rollScores.forEach((rScore) => {
-                score += rScore;
+		this.rollScores.forEach((rScore) => {
+			score += rScore;
 
-                if (rScore === 10) {
-                    score += this.scoreAtRoll(rScoreIndex + 1);
-                    score += this.scoreAtRoll(rScoreIndex + 2);
-                } else {
-                    if (lastRollInFrame) {
-                        if ((rScore + this.scoreAtRoll(rScoreIndex - 1)) === 10) {
-                            score += this.scoreAtRoll(rScoreIndex + 1);
-                        }
-                    }
-                    lastRollInFrame = !lastRollInFrame;
-                }
-                rScoreIndex += 1;
-            }
+			if (rScore === 10) {
+				score += this.scoreAtRoll(rScoreIndex + 1);
+				score += this.scoreAtRoll(rScoreIndex + 2);
+			} else {
+				if (lastRollInFrame) {
+					if ((rScore + this.scoreAtRoll(rScoreIndex - 1)) === 10) {
+						score += this.scoreAtRoll(rScoreIndex + 1);
+					}
+				}
+				lastRollInFrame = !lastRollInFrame;
+			}
+			rScoreIndex += 1;
+		}
         );
 
-        return score;
-    };
+		return score;
+	};
 
-    this.scoreAtRoll = (rollIndex) => {
-        let scoreAtRoll = this.rollScores[rollIndex];
-        if (!scoreAtRoll)
+	this.scoreAtRoll = (rollIndex) => {
+		let scoreAtRoll = this.rollScores[rollIndex];
+		if (!scoreAtRoll) {
             scoreAtRoll = 0;
+        }
 
-        return scoreAtRoll;
-    };
+		return scoreAtRoll;
+	};
 
-    this.rollScores = [];
+	this.rollScores = [];
 }
 
 module.exports = Game;
--- src/FizzBuzzKata.js
+++ src/FizzBuzzKata.js
@@ -1,16 +1,16 @@
 //http://codingdojo.org/cgi-bin/index.pl?KataFizzBuzz
 
 function FizzBuzz(number) {
-    value = RuleMultiple(number, 15, 'FizzBuzz');
-    value = RuleMultiple(value, 5, 'Buzz');
-    return RuleMultiple(value, 3, 'Fizz');
+	let value = RuleMultiple(number, 15, 'FizzBuzz');
+	value = RuleMultiple(value, 5, 'Buzz');
+	return RuleMultiple(value, 3, 'Fizz');
 }
 
 function RuleMultiple(number, divider, return_value){
-    if (number % divider === 0 || number.toString().indexOf(divider.toString()) !== -1) {
-        return return_value;
-    }
-    return number;
+	if (number % divider === 0 || number.toString().indexOf(divider.toString()) !== -1) {
+		return return_value;
+	}
+	return number;
 }
 
 module.exports.FizzBuzz = FizzBuzz;
